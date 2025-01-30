411ef40f0c4d1ab6033b6dc7aee5e49a1422b396
Anastasia Mikhailova <fasechka_99@mail.ru>
2025-01-30 16:59:29 +0300

    исправила ошибку в уроке 1

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const title = "mySecondRepo";
-const screens  = "Простые, Сложные, Интерактивные";
-const screenPrice = 5000;
-const rollback  = 70;
-const fullPrice = 100000;
-const adaptive = true;
+alert("Добро пожаловать!");
+console.log("Пора учиться)");
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
-
-
-console.log(screens.toLowerCase().split(', '));
-
-console.log("Процент отката посреднику за работу " + (fullPrice * (rollback/100)));
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
