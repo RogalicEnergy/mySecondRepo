5791be52b2064808c1f57dbe62fd695ddd8ed9a3
Anastasia Mikhailova <fasechka_99@mail.ru>
2025-02-03 16:49:48 +0300

    Исправила ошибки к уроку №4

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 'use strict'
 
-let title = prompt('Как называется ваш проект?');
-let screens  = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
-let screenPrice = +prompt('Сколько будет стоить данная работа?', "12000");
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-let rollback  = 70; // откат посреднику
-let fullPrice = (screenPrice + servicePrice1 + servicePrice2);  // итоговая стоимость
+let title = prompt('Как называется ваш проект?')
+let screens  = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные")
+let screenPrice = +prompt('Сколько будет стоить данная работа?', "12000")
+let adaptive = confirm('Нужен ли адаптив на сайте?')
+
+let rollback  = 70
+let fullPrice
 let allServicePrices, servicePercentPrice, formattedTitle
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+
+
+const getAllServicePrices = function() {
+  return servicePrice1 + servicePrice2;
+}
 
 const showTypeOf = function(variable) {
   console.log(variable, typeof variable);
 }
 
-const getAllServicePrices = function(allServicePrices) {
-  return screenPrice + servicePrice1 + servicePrice2;
-}
-
-allServicePrices = getAllServicePrices();
-
-function getFullPrice() {
+const getFullPrice = function() {
   return screenPrice + allServicePrices;
 }
 
-fullPrice = getFullPrice();
-
-const getTitle = function(title) {
-  title = title.trim();
-  if (title.length === 0) return '';
-  return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase(); 
+const getTitle = function() {
+  return title.trim()[0].toUpperCase() + title.slice(1).toLowerCase(); 
 };
-
-formattedTitle = getTitle(title);
 
 const getServicePercentPrices = function(servicePercentPrice) {
   return fullPrice - (fullPrice * (rollback / 100));
 }
-
-servicePercentPrice = getServicePercentPrices();
 
 const getRollbackMessage = function(price) {
   switch (true) {
@@ -57,11 +51,17 @@ const getRollbackMessage = function(price) {
   };
 }
 
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle();
+servicePercentPrice = getServicePercentPrices();
+
 showTypeOf(title)
 showTypeOf(screenPrice)
+showTypeOf(adaptive)
 
-console.log(allServicePrices);
-console.log(fullPrice);
-console.log(formattedTitle);
-console.log(Math.ceil(servicePercentPrice));
-console.log(getRollbackMessage(fullPrice));
+console.log(allServicePrices)
+console.log(fullPrice)
+console.log(title)
+console.log(Math.ceil(servicePercentPrice))
+console.log(getRollbackMessage(fullPrice))
